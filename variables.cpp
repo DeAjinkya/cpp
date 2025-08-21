d3f5065de07e068f2e916105fa4e2acd5db20aab
Ajinkya Deshmukh <115269019+DeAjinkya@users.noreply.github.com>
2025-08-21 22:52:50 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 15;
+  cout << myNum << endl;
+  double myFloatNum = 5.99;    // Floating point number (with decimals)
+  cout << myFloatNum << endl;
+  char myLetter = 'D';         // Character
+  cout << myLetter << endl;
+  string myText = "Hello";     // String (text)
+  cout << myText << endl;
+  bool myBoolean = true;       // Boolean (true or false)
+  cout << myBoolean << endl;
+  
+  return 0;
+}
